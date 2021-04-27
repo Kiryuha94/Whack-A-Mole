7f037036de3ea4f6b97984c64dc7719960f58aeb
Пользователь <kirillplusena@gmail.com>
2021-04-27 10:17:06 +0300

    feat: add responsive

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const moles = document.querySelectorAll('.mole');
-const holes = document.querySelectorAll('.hole');
-const scoreTable = document.getElementById('score');
-const button = document.getElementById('button');
-const game = document.getElementById('game');
+const moles = document.querySelectorAll(".mole");
+const holes = document.querySelectorAll(".hole");
+const scoreTable = document.getElementById("score");
+const button = document.getElementById("button");
+const game = document.getElementById("game");
 let score = 0;
 let isTimeOver = false;
 let currMole;
 const DISPLAY_TIME = 700;
-const TIME_ON_GAME = 20000
+const TIME_ON_GAME = 20000;
 
 const chouseMole = (moles) => {
   const index = Math.floor(Math.random() * moles.length);
@@ -21,9 +21,9 @@ const chouseMole = (moles) => {
 
 const showMole = () => {
   const mole = chouseMole(moles);
-  mole.classList.add('up');
+  mole.classList.add("up");
   setTimeout(() => {
-    mole.classList.remove('up');
+    mole.classList.remove("up");
     if (!isTimeOver) showMole();
   }, DISPLAY_TIME);
 };
@@ -32,11 +32,12 @@ const startGame = () => {
   score = 0;
   scoreTable.innerText = score;
   button.disabled = true;
-  button.style.opacity = "0.5"
+  button.style.opacity = "0.5";
+  button.style.cursor = "no-drop";
   isTimeOver = false;
   showMole();
   setTimeout(() => {
-    button.style.opacity = "1"
+    button.style.opacity = "1";
     button.disabled = false;
     isTimeOver = true;
   }, TIME_ON_GAME);
@@ -46,12 +47,12 @@ function kick(e) {
   if (!e.isTrusted) return;
   score += 1;
   scoreTable.innerText = score;
-  this.classList.remove('up');
+  this.classList.remove("up");
 }
 
 window.onload = () => {
-  moles.forEach((mole) => mole.addEventListener('click', kick));
-  button.addEventListener('click', () => {
+  moles.forEach((mole) => mole.addEventListener("click", kick));
+  button.addEventListener("click", () => {
     startGame();
   });
 };
